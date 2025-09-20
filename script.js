9c9eb30bb3f61290cd310f9f53777d7323ad19cc
Omkar Nath <nathomkar799@gmail.com>
2025-09-20 19:01:41 +0530

    Add 80% animations

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-// function circleMouseFollower() {
-//   window.addEventListener("mousemove", function(dets) {
-//     document.querySelector("#minicircle").style.transform =
-//       `translate(${dets.clientX}px, ${dets.clientY}px)`;
-//   });
-// }
+function circleMouseFollower(xscale,yscale) {
+  window.addEventListener("mousemove", function(dets) {
+    document.querySelector("#minicircle").style.transform =
+      `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale},${yscale})`;
+  });
+}
 function firstPageAnim() { 
     var tl = gsap.timeline(); 
     
@@ -35,7 +35,29 @@ function firstPageAnim() {
         ease: Expo.easeInOut
     }) 
     }
+function circleChaptaKaro(){
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+
+    window.addEventListener("mousemove", function(dets){
+        var xdiff = dets.clientX - xprev;
+        var ydiff = dets.clientY - yprev;
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+        
+        xscale = gsap.utils.clamp(.8, 1.2, xdiff)
+        yscale = gsap.utils.clamp(.8, 1.2, ydiff)
+        
+        circleMouseFollower(xscale,yscale);
+        // console.log(gsap.utils.clamp(.8,1.2,xdiff));
+    })
+}
 
 
+circleChaptaKaro();
 // circleMouseFollower();
 firstPageAnim();
