9a9211adabc60cb073e1c1ac1f3b99e34f66d5f6
Omkar Nath <nathomkar799@gmail.com>
2025-09-21 08:00:27 +0530

    Add basic structure and content

--- script.js
+++ script.js
@@ -3,29 +3,23 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-function circleMouseFollower(xscale,yscale) {
-  window.addEventListener("mousemove", function(dets) {
-    document.querySelector("#minicircle").style.transform =
-      `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale},${yscale})`;
-  });
-}
 function firstPageAnim() { 
     var tl = gsap.timeline(); 
     
     tl.from("#nav", {
-         y: '+10', 
-         opacity: 0, 
-         duration: .8, 
-         ease: Expo.easeInOut 
-        })
-
+        y: '+10', 
+        opacity: 0, 
+        duration: .8, 
+        ease: Expo.easeInOut 
+    })
+    
     .to(".boundingelem", {
-         y: '0', 
-         ease: Expo.easeInOut, 
-         duration: 2,
-         delay: -1,
-         stagger: .2 
-        })
+        y: '0', 
+        ease: Expo.easeInOut, 
+        duration: 2,
+        delay: -1,
+        stagger: .2 
+    })
     .from("#herofooter",{
         y: +10,
         opacity: 0,
@@ -35,14 +29,25 @@ function firstPageAnim() {
         ease: Expo.easeInOut
     }) 
     }
+
+function circleMouseFollower(xscale,yscale) {
+    window.addEventListener("mousemove", function(dets) {
+    document.querySelector("#minicircle").style.transform =
+        `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale},${yscale})`;
+    });
+}
+
 function circleChaptaKaro(){
     var xscale = 1;
     var yscale = 1;
-
+    
     var xprev = 0;
     var yprev = 0;
+    let timeout;
 
     window.addEventListener("mousemove", function(dets){
+        clearTimeout(timeout);
+
         var xdiff = dets.clientX - xprev;
         var ydiff = dets.clientY - yprev;
 
@@ -54,10 +59,40 @@ function circleChaptaKaro(){
         
         circleMouseFollower(xscale,yscale);
         // console.log(gsap.utils.clamp(.8,1.2,xdiff));
+
+        timeout = setTimeout(function () {
+            document.querySelector("#minicircle").style.transform =
+      `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`;
+        },100)
     })
 }
 
 
+document.querySelectorAll(".elem").forEach(function (elem) {
+  var rotate = 0;
+  var diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
+
 circleChaptaKaro();
-// circleMouseFollower();
 firstPageAnim();
